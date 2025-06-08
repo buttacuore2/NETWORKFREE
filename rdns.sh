62ca42fe981701635d0c9ba4a09867a15126403c
buttacuore2 <sasijajo2@gmail.com>
2025-06-08 19:49:06 +0100

    Add files via upload

---
+++ rdns.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+ZONE_ID="a3e6bf07fda3ed8e56dde17376255188"
+API_TOKEN="dwBIJbunVoeQ_lzgUs0VPUINO2pxFjxETij4kSb3"
+RECORD_NAME="devpro.andotv.ggff.net"
+RECORD_TYPE="A"
+RECORD_CONTENT="1.1.1.1"
+RECORD_TTL=300
+PROXIED="true"
+API_URL="https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records"
+DATA='{"type":"'"$RECORD_TYPE"'", "name":"'"$RECORD_NAME"'", "content":"'"$RECORD_CONTENT"'", "ttl":"'"$RECORD_TTL"'", "proxied": "'"$PROXIED"'"}'
+
+curl -X POST "$API_URL" \
+     -H "Authorization: Bearer $API_TOKEN" \
+     -H "Content-Type: application/json" \
+     --data "$DATA"
