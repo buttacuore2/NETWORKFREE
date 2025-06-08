f65a1126e906bfa7dbe7e6df3d751d93dcf709d8
buttacuore2 <sasijajo2@gmail.com>
2025-06-08 20:10:42 +0100

    Update rdns.sh

--- rdns.sh
+++ rdns.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 ZONE_ID="a3e6bf07fda3ed8e56dde17376255188"
 API_TOKEN="dwBIJbunVoeQ_lzgUs0VPUINO2pxFjxETij4kSb3"
-RECORD_NAME="devpro.andotv.ggff.net"
+RECORD_NAME="devpro1.andotv.ggff.net"
 RECORD_TYPE="A"
 RECORD_CONTENT="1.1.1.1"
 RECORD_TTL=300
-PROXIED="true"
+PROXIED="false"
 API_URL="https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records"
-DATA='{"type":"'"$RECORD_TYPE"'", "name":"'"$RECORD_NAME"'", "content":"'"$RECORD_CONTENT"'", "ttl":"'"$RECORD_TTL"'", "proxied": "'"$PROXIED"'"}'
+DATA='{"type":"'"$RECORD_TYPE"'", "name":"'"$RECORD_NAME"'", "content":"'"$RECORD_CONTENT"'", "ttl":'$RECORD_TTL', "proxied": '"$PROXIED"'}'
 
 curl -X POST "$API_URL" \
      -H "Authorization: Bearer $API_TOKEN" \
