deaa9edc36d4be2a17a9db1ea3e6ccbdb54b46f9
buttacuore2 <sasijajo2@gmail.com>
2025-06-10 13:44:04 +0100

    Update ssh-vpn.sh

--- ssh/ssh-vpn.sh
+++ ssh/ssh-vpn.sh
@@ -315,6 +315,7 @@ iptables -A FORWARD -m string --algo bm --string "announce" -j DROP
 iptables -A FORWARD -m string --algo bm --string "info_hash" -j DROP
 iptables-save > /etc/iptables.up.rules
 iptables-restore -t < /etc/iptables.up.rules
+apt install netfilter-persistent
 netfilter-persistent save
 netfilter-persistent reload
 
