e88d964d10344fcae3ad569973a2ac64b6804ca1
buttacuore2 <sasijajo2@gmail.com>
2025-06-14 14:40:58 +0100

    Update ssh-vpn.sh

--- ssh/ssh-vpn.sh
+++ ssh/ssh-vpn.sh
@@ -117,7 +117,7 @@ apt install dos2unix -y
 gem install lolcat
 
 # set time GMT +7
-ln -fs /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
+ln -fs /usr/share/zoneinfo/Africa/Casablanca /etc/localtime
 
 # set locale
 sed -i 's/AcceptEnv/#AcceptEnv/g' /etc/ssh/sshd_config
